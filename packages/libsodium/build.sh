6a0b85c578950444f7f0e2de490d9bdb6a7b7678
Mike Sample <msample@users.noreply.github.com>
2026-02-23 10:14:50 -0800

    libsodium: removed ad hoc patch for aarch64 on the release version and, for aarch64 only, pinned it to most recent commit on stable. see https://github.com/jedisct1/libsodium/issues/1509

--- packages/libsodium/build.sh
+++ packages/libsodium/build.sh
@@ -9,21 +9,6 @@ esac
 export CFLAGS="$MARCH -O2 -pipe"
 export CXXFLAGS="${CFLAGS}"
 
-# Fix GCC on aarch64: ipcrypt_armcrypto.c uses signed NEON intrinsics and
-# stores uint8x16_t in uint64x2_t variables. GCC rejects the implicit
-# conversions that Clang allows. Fixed upstream after 1.0.21 (commit 91fe8a46).
-if [ "$(uname -m)" = "aarch64" ]; then
-  # Fix BYTESHL128 macro: use unsigned intrinsics instead of signed
-  sed -i 's/vextq_s8(vdupq_n_s8(0), vreinterpretq_s8_u64/vextq_u8(vdupq_n_u8(0), vreinterpretq_u8_u64/' \
-    src/libsodium/crypto_ipcrypt/ipcrypt_armcrypto.c
-  # Fix pfx_shift_left: use uint8x16_t for byte-level intermediates
-  sed -i 's/const BlockVec shl     /const uint8x16_t shl     /' src/libsodium/crypto_ipcrypt/ipcrypt_armcrypto.c
-  sed -i 's/const BlockVec msb     /const uint8x16_t msb     /' src/libsodium/crypto_ipcrypt/ipcrypt_armcrypto.c
-  sed -i 's/const BlockVec zero    /const uint8x16_t zero    /' src/libsodium/crypto_ipcrypt/ipcrypt_armcrypto.c
-  sed -i 's/const BlockVec carries /const uint8x16_t carries /' src/libsodium/crypto_ipcrypt/ipcrypt_armcrypto.c
-  sed -i 's/vextq_u8(vreinterpretq_u8_u64(msb), zero/vextq_u8(msb, zero/' src/libsodium/crypto_ipcrypt/ipcrypt_armcrypto.c
-fi
-
 ./configure --prefix=/usr --disable-static
 make -j$(nproc)
 make DESTDIR=$OUTPUT_DIR install
