4a72bf9e7008cd0d856cd258db499bd1b7d5a1a3
Mike Sample <msample@users.noreply.github.com>
2026-02-23 13:23:52 -0800

    expect package: now explicitly specifies target so can build on aarch64

--- packages/expect/build.sh
+++ packages/expect/build.sh
@@ -7,14 +7,16 @@ cd expect5.45.4
 patch -Np1 -i ../expect-5.45.4-gcc15-1.patch
 
 case $(uname -m) in
-  x86_64)  MARCH="-march=x86-64-v3" ;;
-  aarch64) MARCH="-march=armv8-a" ;;
-  *)       MARCH="" ;;
+  x86_64)  MARCH="-march=x86-64-v3"; BUILD_TRIPLET="x86_64-unknown-linux-gnu" ;;
+  aarch64) MARCH="-march=armv8-a";   BUILD_TRIPLET="aarch64-unknown-linux-gnu" ;;
+  *)       MARCH="";                 BUILD_TRIPLET="$(uname -m)-unknown-linux-gnu" ;;
 esac
 export CFLAGS="$MARCH -O2 -pipe"
 export CXXFLAGS="${CFLAGS}"
 
 ./configure  --prefix=/usr            \
+            --build="$BUILD_TRIPLET" \
+            --host="$BUILD_TRIPLET"  \
             --with-tcl=/usr/lib     \
             --enable-shared         \
             --disable-rpath         \
