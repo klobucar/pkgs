85f6374c8e8045e7b7bebc42aa886fc3003749bd
Tom <tom@minimal.dev>
2026-02-23 11:48:20 -0800

    alsa-lib: package alsa-lib version 1.2.15.3

---
+++ packages/alsa-lib/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O3 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+./configure --prefix=/usr
+
+make -j$(nproc)
+make check
+make DESTDIR=$OUTPUT_DIR install
