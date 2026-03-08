c832525d26a26fe3d71a3af198eca74b3fad8385
Tom <tom@ciphersink.net>
2026-03-07 19:15:36 -0800

    llvm,zig: capture liblld, build zig from source

--- packages/zig/build.sh
+++ packages/zig/build.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 set -ex
 
+tar -xof "zig-${MINIMAL_ARG_VERSION}.tar.xz"
+cd "zig-${MINIMAL_ARG_VERSION}"
+
 case $(uname -m) in
-  x86_64)  ZIGARCH=x86_64 ;;
-  aarch64) ZIGARCH=aarch64 ;;
-  *)       echo "unsupported architecture: $(uname -m)" >&2; exit 1 ;;
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
 esac
+export CFLAGS="$MARCH -O2 -pipe"
+export CXXFLAGS="${CFLAGS}"
 
-tar -xof "zig-${ZIGARCH}-linux-${MINIMAL_ARG_VERSION}.tar.xz"
-cd "zig-${ZIGARCH}-linux-${MINIMAL_ARG_VERSION}"
+cmake -B build -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_PREFIX_PATH=/usr \
+  -DZIG_SHARED_LLVM=ON \
+  -DZIG_TARGET_MCPU=baseline
 
-mkdir -p $OUTPUT_DIR/usr/{bin,lib/zig,share/doc/zig}
-
-install -m 755 zig $OUTPUT_DIR/usr/bin/zig
-cp -r lib/* $OUTPUT_DIR/usr/lib/zig/
-cp -r doc/* $OUTPUT_DIR/usr/share/doc/zig/
+ninja -C build
+DESTDIR=$OUTPUT_DIR ninja -C build install
