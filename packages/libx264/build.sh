67b22d038ae0992b22bceaa3d5196c3aa3848ad2
Tom <tom@ciphersink.net>
2026-03-11 10:50:34 -0700

    libx264: package libx264 version 0.165.3222

---
+++ packages/libx264/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -ex
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O2 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+./configure \
+  --prefix=/usr \
+  --enable-shared \
+  --disable-static \
+  --enable-pic \
+  --disable-cli
+
+make -j$(nproc)
+make DESTDIR="$OUTPUT_DIR" install
