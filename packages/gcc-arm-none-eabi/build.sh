02b74e053d50e5c2a5a5151d54b7d7020d3f2ac6
Tom <tom@ciphersink.net>
2026-03-27 12:59:28 -0700

    gcc-arm-none-eabi: build common embedded archs instead of everything (~6x faster)

--- packages/gcc-arm-none-eabi/build.sh
+++ packages/gcc-arm-none-eabi/build.sh
@@ -18,6 +18,22 @@ export CXXFLAGS_FOR_TARGET="-g -O2 -ffunction-sections -fdata-sections"
 tar -xof "gcc-${MINIMAL_ARG_GCC_VERSION}.tar.xz"
 tar -xof "newlib-${MINIMAL_ARG_NEWLIB_VERSION}.tar.gz"
 
+# Patch t-rmprofile to only build multilib variants needed for common hobbyist MCUs
+RMPROFILE="gcc-${MINIMAL_ARG_GCC_VERSION}/gcc/config/arm/t-rmprofile"
+sed -i '/^MULTILIB_REQUIRED/d; /^MULTILIB_REUSE/d' "$RMPROFILE"
+cat >> "$RMPROFILE" <<'PATCH'
+# Cortex-M0/M0+ (RP2040, ATSAMD21, STM32F0)
+MULTILIB_REQUIRED += mthumb/march=armv6s-m/mfloat-abi=soft
+# Cortex-M3 (STM32F1 / Blue Pill)
+MULTILIB_REQUIRED += mthumb/march=armv7-m/mfloat-abi=soft
+# Cortex-M4F (ATSAMD51, STM32F4 / Black Pill, nRF52840)
+MULTILIB_REQUIRED += mthumb/march=armv7e-m/mfpu=fpv4-sp-d16/mfloat-abi=hard
+# Cortex-M7 (STM32H7/F7, Teensy 4.x)
+MULTILIB_REQUIRED += mthumb/march=armv7e-m/mfpu=fpv5-d16/mfloat-abi=hard
+# Cortex-M33 (RP2350 / Pico 2, nRF5340)
+MULTILIB_REQUIRED += mthumb/march=armv8-m.main/mfpu=fpv5-sp-d16/mfloat-abi=hard
+PATCH
+
 # Symlink newlib into GCC source tree for unified in-tree build
 ln -s "../newlib-${MINIMAL_ARG_NEWLIB_VERSION}/newlib" "gcc-${MINIMAL_ARG_GCC_VERSION}/newlib"
 ln -s "../newlib-${MINIMAL_ARG_NEWLIB_VERSION}/libgloss" "gcc-${MINIMAL_ARG_GCC_VERSION}/libgloss"
