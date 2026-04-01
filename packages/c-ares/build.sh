64342394edf460936e553b8e69ce72dba98c2161
Tom <tom@ciphersink.net>
2026-03-31 18:21:34 -0700

    c-ares: package c-ares version 1.34.6

---
+++ packages/c-ares/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -ex
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export SOURCE_DATE_EPOCH=0
+export CFLAGS="$MARCH -O2 -pipe -gno-record-gcc-switches -ffile-prefix-map=$(pwd)=/builddir"
+export LDFLAGS="-Wl,--build-id=none"
+export CXXFLAGS="${CFLAGS}"
+
+cmake -B build \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+  -DCARES_BUILD_TOOLS=OFF \
+  -DCARES_SHARED=ON \
+  -DCARES_STATIC=OFF \
+  -DCARES_BUILD_TESTS=OFF
+
+cmake --build build -j$(nproc)
+DESTDIR=$OUTPUT_DIR cmake --install build
